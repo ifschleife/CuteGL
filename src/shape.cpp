813a10bfaefd4a7b94d2c2ddf8a9c1ee90b63f9d
Daniel Rahier <daniel@rahier.biz>
2017-02-05 12:18:59 +0100

    Moved implementation to cpp file

--- src/shape.cpp
+++ src/shape.cpp
@@ -93,3 +93,145 @@ void Shape::sub_divide(uint_fast8_t level)
         indices = new_faces;
     }
 }
+
+
+RenderObject::RenderObject()
+{
+    initializeOpenGLFunctions();
+}
+
+void RenderObject::addFace(const std::array<uint32_t, 3>&& indices)
+{
+    m_mesh.indices.emplace_back(indices);
+}
+
+void RenderObject::addVertex(const Vec3D&& vertex)
+{
+    m_mesh.positions.emplace_back(vertex);
+}
+
+void RenderObject::setMesh(const Mesh& mesh)
+{
+    m_mesh = mesh;
+}
+
+void RenderObject::initGL()
+{
+    // vbo for vertex positions
+
+    glGenBuffers(1, &m_mesh.vbos.pos);
+    glBindBuffer(GL_ARRAY_BUFFER, m_mesh.vbos.pos);
+    glBufferData(GL_ARRAY_BUFFER, m_mesh.positions.size() * sizeof(m_mesh.positions[0]), m_mesh.positions.data(),
+                 GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // vbo for vertex indices
+
+    glGenBuffers(1, &m_mesh.vbos.index);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_mesh.vbos.index);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_mesh.indices.size() * sizeof(m_mesh.indices[0]), m_mesh.indices.data(),
+                 GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    m_shader.create_uniform_block((void*)&m_model_matrix, sizeof(m_model_matrix));
+
+    if (!m_shader.link())
+    {
+        qDebug() << m_shader.log();
+    }
+}
+
+void RenderObject::rotate(float angle)
+{
+    m_model_matrix.rotate(angle, {1.0f, 0.0f, 0.0f});
+}
+
+void RenderObject::translate(float x, float y, float z)
+{
+    m_model_matrix.translate(x, y, z);
+}
+
+void RenderObject::setFragmentShader(const QString&& filename)
+{
+    m_shader.addShaderFromSourceFile(QOpenGLShader::Fragment, filename);
+}
+
+void RenderObject::setVertexShader(const QString&& filename)
+{
+    m_shader.addShaderFromSourceFile(QOpenGLShader::Vertex, filename);
+}
+
+void RenderObject::setTexture(const std::unique_ptr<QImage>&& texture)
+{
+    glPixelStorei(GL_PACK_ALIGNMENT, 1); // for byte textures
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    glGenTextures(1, &m_mesh.vbos.texture);
+    glBindTexture(GL_TEXTURE_2D, m_mesh.vbos.texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+                 texture->width(), texture->height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, texture->rgbSwapped().bits());
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void RenderObject::setCullFaceMode(bool mode)
+{
+    m_cull_faces = mode;
+}
+
+void RenderObject::setWireframeMode(bool mode)
+{
+    m_show_wireframe = mode;
+}
+
+void RenderObject::render(const QMatrix4x4& pv)
+{
+    const QMatrix4x4 mvp = pv * m_model_matrix;
+
+    m_shader.bind();
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, m_mesh.vbos.pos);
+    glVertexAttribPointer(m_shader.attributeLocation("position"), 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    m_shader.set_uniform_block_data((void*)&mvp, sizeof(mvp));
+
+    if (m_mesh.vbos.texture)
+    {
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, m_mesh.vbos.texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    }
+
+    if (m_cull_faces)
+        glEnable(GL_CULL_FACE);
+    glPolygonMode(GL_FRONT_AND_BACK, m_show_wireframe ? GL_LINE : GL_FILL);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_mesh.vbos.index);
+    glDrawElements(GL_TRIANGLES, 3 * (GLsizei)m_mesh.indices.size(), GL_UNSIGNED_INT, nullptr);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    m_shader.release();
+    glDisableVertexAttribArray(1);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glDisable(GL_BLEND);
+    glDisable(GL_CULL_FACE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void RenderObject::setAnimRotation(float angle)
+{
+    m_anim_rotation = angle;
+}
+
+void RenderObject::animate()
+{
+    rotate(m_anim_rotation);
+}
--- src/shape.h
+++ src/shape.h
@@ -55,145 +55,27 @@ using Mesh = Shape;
 class RenderObject : public QOpenGLFunctions_4_5_Core
 {
 public:
-    RenderObject()
-    {
-        initializeOpenGLFunctions();
-    }
+    RenderObject();
 
-    void addFace(const std::array<uint32_t, 3>&& indices)
-    {
-        m_mesh.indices.emplace_back(indices);
-    }
+    void addFace(const std::array<uint32_t, 3>&& indices);
+    void addVertex(const Vec3D&& vertex);
 
-    void addVertex(const Vec3D&& vertex)
-    {
-        m_mesh.positions.emplace_back(vertex);
-    }
+    void initGL();
+    void animate();
+    void render(const QMatrix4x4& pv);
 
-    void setMesh(const Mesh& mesh)
-    {
-        m_mesh = mesh;
-    }
+    void rotate(float angle);
+    void translate(float x, float y, float z);
 
-    void initGL()
-    {
-        // vbo for vertex positions
+    void setAnimRotation(float angle);
 
-        glGenBuffers(1, &m_mesh.vbos.pos);
-        glBindBuffer(GL_ARRAY_BUFFER, m_mesh.vbos.pos);
-        glBufferData(GL_ARRAY_BUFFER, m_mesh.positions.size() * sizeof(m_mesh.positions[0]), m_mesh.positions.data(),
-                     GL_STATIC_DRAW);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    void setFragmentShader(const QString&& filename);
+    void setVertexShader(const QString&& filename);
 
-        // vbo for vertex indices
-
-        glGenBuffers(1, &m_mesh.vbos.index);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_mesh.vbos.index);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_mesh.indices.size() * sizeof(m_mesh.indices[0]), m_mesh.indices.data(),
-                     GL_STATIC_DRAW);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-        m_shader.create_uniform_block((void*)&m_model_matrix, sizeof(m_model_matrix));
-
-        if (!m_shader.link())
-        {
-            qDebug() << m_shader.log();
-        }
-    }
-
-    void rotate(float angle)
-    {
-        m_model_matrix.rotate(angle, {1.0f, 0.0f, 0.0f});
-    }
-
-    void translate(float x, float y, float z)
-    {
-        m_model_matrix.translate(x, y, z);
-    }
-
-    void setFragmentShader(const QString&& filename)
-    {
-        m_shader.addShaderFromSourceFile(QOpenGLShader::Fragment, filename);
-    }
-
-    void setVertexShader(const QString&& filename)
-    {
-        m_shader.addShaderFromSourceFile(QOpenGLShader::Vertex, filename);
-    }
-
-    void setTexture(const std::unique_ptr<QImage>&& texture)
-    {
-        glPixelStorei(GL_PACK_ALIGNMENT, 1); // for byte textures
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-        glGenTextures(1, &m_mesh.vbos.texture);
-        glBindTexture(GL_TEXTURE_2D, m_mesh.vbos.texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
-                     texture->width(), texture->height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, texture->rgbSwapped().bits());
-        glGenerateMipmap(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
-
-    void setCullFaceMode(bool mode)
-    {
-        m_cull_faces = mode;
-    }
-
-    void setWireframeMode(bool mode)
-    {
-        m_show_wireframe = mode;
-    }
-
-    void render(const QMatrix4x4& pv)
-    {
-        const QMatrix4x4 mvp = pv * m_model_matrix;
-
-        m_shader.bind();
-
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, m_mesh.vbos.pos);
-        glVertexAttribPointer(m_shader.attributeLocation("position"), 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-        m_shader.set_uniform_block_data((void*)&mvp, sizeof(mvp));
-
-        if (m_mesh.vbos.texture)
-        {
-            glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, m_mesh.vbos.texture);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        }
-
-        if (m_cull_faces)
-            glEnable(GL_CULL_FACE);
-        glPolygonMode(GL_FRONT_AND_BACK, m_show_wireframe ? GL_LINE : GL_FILL);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_mesh.vbos.index);
-        glDrawElements(GL_TRIANGLES, 3 * (GLsizei)m_mesh.indices.size(), GL_UNSIGNED_INT, nullptr);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-        m_shader.release();
-        glDisableVertexAttribArray(1);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        glDisable(GL_BLEND);
-        glDisable(GL_CULL_FACE);
-    }
-
-    void setAnimRotation(float angle)
-    {
-        m_anim_rotation = angle;
-    }
-
-    void animate()
-    {
-        rotate(m_anim_rotation);
-    }
+    void setCullFaceMode(bool mode);
+    void setMesh(const Mesh& mesh);
+    void setTexture(const std::unique_ptr<QImage>&& texture);
+    void setWireframeMode(bool mode);
 
 private:
     Mesh m_mesh;
